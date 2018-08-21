728f49d6f28b19c2692b984171892d2b97fe4ab1
Koushik Meenakshisundaram <koushik.ms@gmail.com>
2018-08-21 18:47:28 +0530

    Make guess.sh work in linux

--- guess.sh
+++ guess.sh
@@ -1,1 +1,7 @@
-docker run -v $1:/tf_files -v $2:/img/guess.jpg  xblaster/tensor-guess sh -c "./guess.sh" 2> /dev/null
+# docker run -v $1:/tf_files -v $2:/img/guess.jpg  xblaster/tensor-guess sh -c "./guess.sh" 2> /dev/null
+
+docker run --rm \
+  -v $1:/tf_files \
+  -v $2:/img/guess.jpg \
+  xblaster/tensor-guess \
+  sh -c "cp label_image.py /tf_files && cd /tf_files && python label_image.py /img/guess.jpg"
